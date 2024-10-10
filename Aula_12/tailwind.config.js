6dc9fc335306fa0948678ba45dbf3b1a3607f7fd
Clécio Lira <cclecioc.com@gmail.com>
2024-10-10 20:55:45 -0300

    update curso tailwind

---
+++ Aula_12/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: 'media',
+  theme: {
+    extend: {
+      colors: {
+        brand: '#cf42f5'
+      }
+    },
+  },
+  plugins: [],
+}
